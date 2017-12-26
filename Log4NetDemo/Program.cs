e6c430e62bc5ef08ebfddeb8286e00bfebcfc957
givemin5 <givemin5@gmail.com>
2017-12-26 16:05:47 +0800

    加入專案檔案。

---
+++ Log4NetDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4NetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var thisPath = System.IO.Directory.GetCurrentDirectory();
+            string log4netPath =  $"{thisPath}\\log4net.config";
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(new System.IO.FileInfo(log4netPath));
+            ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            logger.Info("Hello World");
+        }
+
+    }
+}
